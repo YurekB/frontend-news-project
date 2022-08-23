3d54516d407d2b3ff6559a0e8a3f084233cc50b2
YurekB <boguckiyurek@gmail.com>
2022-08-23 16:15:19 +0100

    trello7

--- yureks-news/src/components/IndividualArticle.jsx
+++ yureks-news/src/components/IndividualArticle.jsx
@@ -1,18 +1,39 @@
 import { useParams } from "react-router-dom";
-import { getArticleById } from "../functions/functions";
+import { getArticleById, addArticleLike } from "../functions/functions";
 import { useEffect, useState } from "react";
-import Comments from "./Comments";
+// import Comments from "./Comments";
 
 const IndividualArticle = () => {
   const [article, setArticle] = useState({});
-
   const { article_id } = useParams();
 
   useEffect(() => {
     getArticleById(article_id).then((res) => {
       setArticle(res);
     });
-  }, []);
+  }, [article_id]);
+
+  const VotesAdd = () => {
+    const addLike = { inc_votes: 1 };
+    addArticleLike(article_id, addLike)
+      .then((res) => {
+        setArticle(res);
+      })
+      .catch((err) => {
+        return <p>{err}</p>;
+      });
+  };
+
+  const VotesRemove = () => {
+    const RemoveLike = { inc_votes: -1 };
+    addArticleLike(article_id, RemoveLike)
+      .then((res) => {
+        setArticle(res);
+      })
+      .catch((err) => {
+        return <p>{err}</p>;
+      });
+  };
 
   return (
     <div className="ArticlePage">
@@ -23,10 +44,10 @@ const IndividualArticle = () => {
         <p className="ArticleAuthor">Written by: {article.author}</p>
         <p className="ArticleDate">{article.created_at}</p>
         <p className="ArticleVoted">{article.votes}</p>
-        <button>Like</button>
-        <button>Dislike</button>
+        <button onClick={VotesAdd}>Like</button>
+        <button onClick={VotesRemove}>Dislike</button>
       </div>
-      <Comments article_id={article_id} />
+      {/* <Comments article_id={article_id} /> */}
     </div>
   );
 };
